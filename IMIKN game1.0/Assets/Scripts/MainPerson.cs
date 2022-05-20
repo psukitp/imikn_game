6c54687d748f89a05d9cc76d4da78007f71669e7
psukitp <jfellertv@gmail.com>
2022-05-21 01:42:58 +0500

    download to git

---
+++ IMIKN game1.0/Assets/Scripts/MainPerson.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPerson : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction;
+    private Rigidbody2D rigidBody;
+    public float scale;
+    private Animator anim;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        speed = 20;
+        scale = transform.localScale.x;
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        if (direction.x == 1) transform.localScale = new Vector2(-scale, transform.localScale.y);
+        else if (direction.x == -1) transform.localScale = new Vector2(scale, transform.localScale.y);
+        direction.y = Input.GetAxisRaw("Vertical");
+        if (direction.x != 0 || direction.y != 0)
+        {
+            anim.SetInteger("moving", 1);
+        }
+        else anim.SetInteger("moving", 0);
+
+        rigidBody.MovePosition(rigidBody.position + direction * speed*Time.deltaTime);
+    }
+}
---
+++ IMIKN game1.0/Assets/Scripts/NPSMoving.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime;
+
+public class NPSMoving : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D body;
+    public float horizontalMoving;
+    public float verticalMoving;
+    public Vector2 direction;
+    private string typeOfMove;
+    public float scale;
+    private Animator anim;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        var moving = -1 * speed * Time.deltaTime;
+        horizontalMoving = moving;
+        verticalMoving = body.velocity.y;
+        direction = -transform.right;
+        Debug.DrawRay(transform.position, direction);
+        typeOfMove = "horizontal";
+        scale = transform.localScale.x;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Ray2D ray = new Ray2D();
+        ray.origin = transform.position;
+        ray.direction = direction;
+        anim.SetInteger("stay", 1);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 0.5f);
+        if(hit!=null && hit.collider!=null && hit.collider.gameObject.GetComponent<PolygonCollider2D>()!=null)
+        {
+            if(typeOfMove=="vertical")
+            {
+                if (Random.Range(0, 2) == 0)
+                {
+                    horizontalMoving = -1;
+                    verticalMoving = 0;
+                    direction = -transform.right;
+                    typeOfMove = "horizontal";
+                    transform.localScale = new Vector2(scale, transform.localScale.y);
+                }
+                else
+                {
+                    horizontalMoving = 1;
+                    verticalMoving = 0;
+                    direction = transform.right;
+                    typeOfMove = "horizontal";
+                    transform.localScale = new Vector2(-scale, transform.localScale.y);
+                }
+            }
+            else
+            {
+                if (Random.Range(0, 2) == 0)
+                {
+                    horizontalMoving = 0;
+                    verticalMoving = -1;
+                    direction = -transform.up;
+                    typeOfMove = "vertical";
+                }
+                else
+                {
+                    horizontalMoving = 0;
+                    verticalMoving = 1;
+                    direction = transform.up;
+                    typeOfMove = "vertical";
+                   
+                }
+            }
+        }
+        Debug.Log(verticalMoving + " " + horizontalMoving);
+        Vector2 movement = new Vector2(horizontalMoving, verticalMoving);
+        body.velocity = movement;
+    }
+}
