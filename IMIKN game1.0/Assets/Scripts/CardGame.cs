421844934a842b3ca6d60125b19420fca93ade79
psukitp <jfellertv@gmail.com>
2022-05-29 17:29:30 +0500

    Recreate first quest

--- IMIKN game1.0/Assets/Scripts/CardGame.cs
+++ IMIKN game1.0/Assets/Scripts/CardGame.cs
@@ -3,62 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardGame : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class CardGame : MonoBehaviour
 {
-    public float timeSpend;
-
-    private bool isDragging;
-    private Vector3 oldPosotion;
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isDragging = true;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        isDragging = false;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (isDragging)
-        {
-            timeSpend += Time.deltaTime;
-            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            gameObject.transform.position = new Vector3(transform.position.x + point.x, transform.position.y, transform.position.z);
-        }
-        else
-        {
-            if (transform.position.x > 3.5f)
-            {
-                print(transform.position.x);
-                gameObject.transform.position += new Vector3(-6f * Time.deltaTime, 0, 0);
-            }
-        }
-    }
-    private void OnMouseDrag()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x - oldPosotion.x, transform.position.y, transform.position.z);
-    }
-
+    private Vector3 dragOffset;
+    private float speedDrag = 20f;
     private void OnMouseDown()
     {
-        timeSpend = 0;
-        Vector3 delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        isDragging = true;
+        dragOffset = this.transform.position - GetMousePosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        var newPos = GetMousePosition() + dragOffset;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, newPos, speedDrag * Time.deltaTime);  
+    }
+    private Vector3 GetMousePosition()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = -1;
+        mousePos.y = this.transform.position.y;
+        return mousePos;
     }
 
     private void OnMouseUp()
     {
-        timeSpend = 0;
-        isDragging = false;
+        this.transform.position = new Vector3(-21, this.transform.position.y, -1);
     }
 }
